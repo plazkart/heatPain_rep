2573818d0ec81b3914f2b2d89578ec8a84a746e5
plazkart <44684286+plazkart@users.noreply.github.com>
2023-12-06 13:03:18 +0300

    Hello world
    funtions are loaded to github separate project

---
+++ hp_default.m
@@ -0,0 +1,6 @@
+%This script loads mainStruct and sets main directory
+action = 'load';
+mainStruct = hp_make(action);
+
+%set path to folder in which all data is located
+mainStruct.meta.folder = 'G:\_other\fMRS-heatPain';
