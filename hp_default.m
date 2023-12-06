96dffc9c51e635cc8a56ceff2a671694dfbd9856
plazkart <44684286+plazkart@users.noreply.github.com>
2023-12-06 13:27:20 +0300

    Update hp_default.m
    Make hp_default independent of hp_make

--- hp_default.m
+++ hp_default.m
@@ -1,6 +1,7 @@
 %This script loads mainStruct and sets main directory
-action = 'load';
-mainStruct = hp_make(action);
 
 %set path to folder in which all data is located
-mainStruct.meta.folder = 'G:\_other\fMRS-heatPain';
+mainFolder = 'G:\_other\fMRS-heatPain';
+load([mainFolder '\_meta\structMeta.mat']);
+
+mainStruct.meta.folder = mainFolder;
